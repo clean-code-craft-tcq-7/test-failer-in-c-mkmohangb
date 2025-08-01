fc0c3d8b8c5a1426171fb8c79aeadd472f439de2
Manoj Mohan <haimanojkumar@gmail.com>
2025-08-01 10:55:08 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -7,7 +7,7 @@ char size(int cms) {
         sizeName = 'S';
     } else if (cms > 38 && cms < 42) {
         sizeName = 'M';
-    } else if (cms > 42) {
+    } else if (cms >= 42) {
         sizeName = 'L';
     }
     return sizeName;
