a48b0399feaf9031218a6121f993b7f617be0fce
Manoj Mohan <haimanojkumar@gmail.com>
2025-08-01 12:28:12 +0530

    added test to uncover issue

--- tshirts.c
+++ tshirts.c
@@ -16,6 +16,7 @@ char size(int cms) {
 int testTshirtSize() {
     printf("\nTshirt size test\n");
     assert(size(37) == 'S');
+    assert(size(38) == 'M'); // sports/athletic fit - S, casual - 'M'
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
